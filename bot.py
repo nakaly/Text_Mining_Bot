350853b068d22945274448eac20b2b99891eba50
unknown <nakamurr@NAKAMURR1.asiapacific.hpqcorp.net>
2014-09-17 17:30:18 +0800

    write tweets to the website

--- bot.py
+++ bot.py
@@ -26,7 +26,9 @@ class TwitterAPI:
 
 if __name__ == "__main__":
     twitter = TwitterAPI()
-    twitter.tweet("Hello world!") #You probably want to remove this line
-    while True:
-        #Send a tweet here!
+    #twitter.tweet("Hello world!") #You probably want to remove this line
+    public_tweets = twitter.home_timeline()
+    twitter.tweet("Hello world!")
+    return str(public_tweets)
+
         time.sleep(60)
