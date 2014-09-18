cc708fd77153143a9abffaa44e82fc374e0ba33a
unknown <nakamurr@NAKAMURR1.asiapacific.hpqcorp.net>
2014-09-18 13:23:04 +0800

    debug 16

--- bot.py
+++ bot.py
@@ -64,16 +64,16 @@ def index():
             wordcount_old[word] = 1
 
     wordcount_diff = {}
-    for k, v in wordcount:
+    for k, v in wordcount.iteritems():
         print k, v
-#        found = False
-#        for k_old, v_old in wordcount_old:
-#            if k == k_old:
-#                diff = abs(v - v_old)
-#                wordcount_diff[k] = diff
-#                found = True
-#        if not found:
-#            wordcount_diff[k] = v
+        found = False
+        for k_old, v_old in wordcount_old.iteritems():
+            if k == k_old:
+                diff = abs(v - v_old)
+                wordcount_diff[k] = diff
+                found = True
+        if not found:
+            wordcount_diff[k] = v
 
 
 
