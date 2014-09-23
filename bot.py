331027381bccef88e808de55a8795241c259cc0a
unknown <nakamurr@NAKAMURR1.asiapacific.hpqcorp.net>
2014-09-23 14:13:48 +0800

    debug 1

--- bot.py
+++ bot.py
@@ -93,8 +93,8 @@ def preparation_freq_words():
 def freq_words():
 	response.content_type = 'application/json'
 	dict_freq_words = {}
-	wordID = int(request.GET.get("id"))
-	wordCount = int(request.GET.get("count"))
+	wordID = int(request.query.id)
+	wordCount = int(request.query.count)
 	redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 	redis_db = redis.from_url(redis_url)
 	freq_words = redis_db.zrange("freq_word", 0, -1, withscores=True)
