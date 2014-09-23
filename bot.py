8eba2ad1e3ef17bab2ea8deddccd312cc3f336bb
unknown <nakamurr@NAKAMURR1.asiapacific.hpqcorp.net>
2014-09-23 12:49:04 +0800

    debug 13

--- bot.py
+++ bot.py
@@ -95,7 +95,7 @@ def freq_words():
 	dict_freq_words = {}
 	redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 	redis_db = redis.from_url(redis_url)
-	freq_words = redis_db.zrange("freg_word", 0, -1, withscores=True)
+	freq_words = redis_db.zrange("freq_word", 0, -1, withscores=True)
 	dict_freq_words = dict(zip(freq_words))
 	return dict_freq_words
 
