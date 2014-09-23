b1d4a4f8977d6c840885df20e7ba26cc63d7f1e2
unknown <nakamurr@NAKAMURR1.asiapacific.hpqcorp.net>
2014-09-23 12:41:50 +0800

    debug 8

--- bot.py
+++ bot.py
@@ -93,9 +93,9 @@ def preparation_freq_words():
 def freq_words():
 	response.content_type = 'application/json'
 	dict_freq_words = {}
-#    redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-#    redis = redis.from_url(redis_url)
-	freq_words = redis.zrange("freg_word", 0, -1, withscores=True)
+    redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+    redis_db = redis.from_url(redis_url)
+	freq_words = redis_db.zrange("freg_word", 0, -1, withscores=True)
 	dict_freq_words = dict(zip(freq_words))
 	return dict_freq_words
 
