a27f596eb469814a0ee6884cf756e28261370686
unknown <nakamurr@NAKAMURR1.asiapacific.hpqcorp.net>
2014-09-23 12:18:35 +0800

    debug 3

--- bot.py
+++ bot.py
@@ -79,8 +79,9 @@ def preparation_freq_words():
         if not found:
             wordcount_diff[k] = v
 
-     redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-     redis = redis.from_url(redis_url)
+    redistogo_url == None: redis_url = '127.0.0.1:6379' else: redis_url = redistogo_url redis_url = redis_url.split('redis://redistogo:')[1] redis_url = redis_url.split('/')[0] REDIS_PWD, REDIS_HOST = redis_url.split('@', 1) redis_url = "%s?password=%s" % (REDIS_HOST, REDIS_PWD) session_opts = { 'session.type': 'redis', 'session.url': redis_url, 'session.data_dir': './cache/', 'session.key': 'appname', 'session.auto': True, }
+    redis = redis.from_url(redis_url)
+
 
     for k, v in wordcount_diff.iteritems:
     	redis.zadd("freq_word", k, v)
